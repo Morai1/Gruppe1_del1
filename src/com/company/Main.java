b7fe6aefd7ed4278502ae2bd4a6e7086ee7d5681
Luciachristiani <luciachristiani@gmail.com>
2021-09-28 12:07:46 +0200

    Commit 10

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,7 +12,23 @@ public class Main {
 int terning1;
 int terning2;
 
-// Besked til brugeren ved spillets start.
+// Besked til spillerne ved spillets start.
         System.out.println("Hej og velkommen. Reglerne er simple. Den spiller der først opnår 40 point eller derover vinder");
+
+        // her er det meningen at randomizeren skal virke
+ int terning1= nextInt(int n);
+ int terning2= nextInt(int n);
+
+ // her plusses værdien af de to terninger og tillæges ens totale score
+   getSum():int terning2 + terning1;
+
+   Total= total +
+
+
+
+
+
+
+
     }
 }
