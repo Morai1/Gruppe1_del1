b70928beb8b690d9a405d6ed7672877fd70cf7c4
s007744 <s007744@dtu.dk>
2021-09-28 10:12:01 +0200

    Commit 5

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,13 @@
+import java.util.Random;
 package com.company;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        //Vi skal have lavet en master/development branch
+        // getSum():int metode skal laves
+
+
     }
 }
