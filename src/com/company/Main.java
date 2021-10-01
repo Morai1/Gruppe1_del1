815e718d71f976abdc035de48118ecfc4c1be9bb
jasminclemmensen <s215726@dtu.dk>
2021-10-01 13:11:10 +0200

    Commit 20

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,24 +1,15 @@
 package com.company;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
-
 public class Main {
     private static void getSum() {
         
     }
     public static void main(String[] args) {
-	// write your code here
-        //Vi skal have lavet en master/development branch
-        // getSum():int metode skal laves
-
-        // de to terninger
-         {
-        }
 
 // de to spillere
 int spiller1;
 int spiller2;
+
 // Besked til brugeren ved spillets start.
         System.out.println("Hej og velkommen. Reglerne er simple. Den spiller der først opnår 40 point eller derover vinder");
 
@@ -27,12 +18,11 @@ int spiller2;
         terning1 = (int)(Math.random()*6+1);
  int terning2;
         terning2 = (int)(Math.random()*6+1);
- AtomicInteger sum = new AtomicInteger();
-        sum.set(terning1 + terning2);
+        int sum = 0;
+        sum = (terning1 + terning2);
 
         // her plusses værdien af de to terninger og oplyses til spilleren
         System.out.println("Du slog = "+sum);
-
 
         ///her tillæges værdien af de to terninger og totalscoren og oplyses til spilleren
 int Total = 0;
