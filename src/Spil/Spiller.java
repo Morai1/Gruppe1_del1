0b3ea3629844ca3d783633d237c57eb1d8c83311
Mohamad Meri <moraimohamed7@gmail.com>
2021-10-01 13:58:41 +0200

    Commit23

--- src/Spil/Spiller.java
+++ src/Spil/Spiller.java
@@ -3,13 +3,17 @@ package Spil;
 public class Spiller {
 
 
-        private String Spillernavn, Spillernummer;
+         String Spillernavn, Spillernummer;
+     String spillernavn;
+     String spillernummer;
+    private Spiller spiller1;
+    private Spiller spiller2;
 
-
-        public Spiller (String Spillernavn, String Spillernummer)
+    public Spiller (String Spillernavn, String Spillernummer)
         {
-            Spillernavn = Spillernavn;
-            Spillernummer = Spillernummer;
+            spillernavn = Spillernavn;
+            spillernummer = Spillernummer;
+
 
         }
 
@@ -22,6 +26,22 @@ public class Spiller {
 
             return result;
         }
+
+    public void setSpiller(Spiller spiller1) {
+        this.spiller1 = spiller1;
     }
+
+    public Spiller getSpiller1() {
+        return spiller1;
+    }
+
+    public Spiller getSpiller2() {
+        return spiller2;
+    }
+
+    public void setSpiller2(Spiller spiller2) {
+        this.spiller2 = spiller2;
+    }
+}
 
 
