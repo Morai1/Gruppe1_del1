688f879417c9ef402803cb3f598352dcc5e80130
Mohamad Meri <moraimohamed7@gmail.com>
2021-10-01 13:25:10 +0200

    Commit21

--- src/Spil/Spiller.java
+++ src/Spil/Spiller.java
@@ -2,4 +2,26 @@ package Spil;
 
 public class Spiller {
 
-}
+
+        private String Spillernavn, Spillernummer;
+
+
+        public Spiller (String Spillernavn, String Spillernummer)
+        {
+            Spillernavn = Spillernavn;
+            Spillernummer = Spillernummer;
+
+        }
+
+        public String toString()
+        {
+            String result;
+
+            result = Spillernavn + "\n";
+            result += Spillernummer;
+
+            return result;
+        }
+    }
+
+
