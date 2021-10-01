e1c10e3eaf646a56d817a2f32982cbc090745336
jasminclemmensen <s215726@dtu.dk>
2021-10-01 09:45:37 +0200

    Commit 18

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -37,11 +37,13 @@ int spiller2;
         ///her tillæges værdien af de to terninger og totalscoren og oplyses til spilleren
 int Total = 0;
    Total= Total + terning1 + terning2;
+
         System.out.println("Din totale score er nu "+Total);
 
 
 // find ud af hvordan man får spillet til at stoppe, når ens score er 40 eller derover
         // loop evt. while - while total<40 do...
+        if (Total>40)
         System.out.println("Tillykke. Du har vundet spillet");
 
 
